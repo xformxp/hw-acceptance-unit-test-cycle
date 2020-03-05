1db976c69fef3169f96949431a6ff7b40630439d
EC2 Default User <ec2-user@ip-172-31-44-239.us-east-2.compute.internal>
2020-03-05 21:17:58 +0000

    director field added

--- rottenpotatoes/db/seeds.rb
+++ rottenpotatoes/db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+=begin
+Original data
 movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
     	  {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
     	  {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
@@ -17,6 +19,21 @@ movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
       	  {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
       	  {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
   	 ]
+=end
+
+movies = [{:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director => 'Guy Ritchie'},
+{:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984', :director => 'James Cameron'},
+{:title => 'Avatar', :rating => 'PG-13', :release_date => '20-Dec-2009', :director => 'James Cameron'},
+{:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989', :director => 'Rob Reiner'},
+{:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011', :director => 'Tate Taylor'},
+{:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001', :director => 'Lasse Hallström'},
+{:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001', :director => 'Jean-Pierre Jeunet'},
+{:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968', :director => 'Stanley Kubrick'},
+{:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004', :director => 'The Incredibles'},
+{:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981', :director => 'Steven Spielberg'},
+{:title => 'Catch Me If You Can', :rating => 'PG-13', :release_date => '15-Dec-2002', :director => 'Steven Spielberg'},
+{:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000', :director => 'Nick Park'},
+]
 
 movies.each do |movie|
   Movie.create!(movie)
